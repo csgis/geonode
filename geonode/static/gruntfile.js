2fc678f94f63b517b8519c436ba3bdc7e505e324
Toni <toni.schoenbuchner@csgis.de>
2020-10-29 11:44:28 +0100

    Update gruntfile.js

--- geonode/static/gruntfile.js
+++ geonode/static/gruntfile.js
@@ -295,5 +295,7 @@ module.exports = function(grunt) {
   // build production
   grunt.registerTask('production', ['jshint', /*'clean:lib',*/ 'less:production', 'concat:bootstrap', 'copy', 'replace', 'cssmin', 'uglify:production', 'babel']);
   grunt.registerTask('build-less-prod', ['less:production']);
+  grunt.registerTask('default', ['jshint', /*'clean:lib',*/ 'less:production', 'concat:bootstrap', 'copy', 'replace', 'cssmin', 'uglify:production', 'babel']);
+
 
 };
